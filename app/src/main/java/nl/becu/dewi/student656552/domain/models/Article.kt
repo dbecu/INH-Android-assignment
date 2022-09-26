2033b575b8d07edd12ee42fa79c1041779b78859
Dewi <dewi.becu@gmail.com>
2022-09-26 19:12:42 +0200

    First commit

---
+++ app/src/main/java/nl/becu/dewi/student656552/domain/models/Article.kt
@@ -0,0 +1,9 @@
+package nl.becu.dewi.student656552.domain.models
+
+data class Article(
+    val Id: Int,
+    val Feed: Int,
+    val Title: String,
+    val Summary: String,
+    val Image: String
+)
