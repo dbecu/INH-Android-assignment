08afd58b30d384601c3958e4e13328da73a30d83
Dewi <dewi.becu@gmail.com>
2022-10-03 16:13:13 +0200

    bottom nav bar has profile now

--- app/src/main/java/nl/becu/dewi/student656552/articles/presentation/screens/DefaultScreen.kt
+++ app/src/main/java/nl/becu/dewi/student656552/articles/presentation/screens/DefaultScreen.kt
@@ -1,9 +1,24 @@
 package nl.becu.dewi.student656552.articles.presentation.screens
 
 import android.util.Log.d
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -13,22 +28,53 @@ fun DefaultScreen(
     navController: NavController,
     content: @Composable() () -> Unit,
 ) {
-    Scaffold (
-        topBar = {
-            TopAppBar(
-                title = { Text(navigationTitle) },
-                backgroundColor = MaterialTheme.colors.primary,
-                contentColor = MaterialTheme.colors.onPrimary,
-                navigationIcon = {
-                    if (haveBackButton) {
-                        IconButton(onClick = { navController.popBackStack() }) {
-                            //TODO: Add an icon
-                            Text("Back")
-                        }
-                    }
+    Scaffold(
+        topBar = { TopBar(navigationTitle, haveBackButton, navController) },
+        bottomBar = { BottomBar() }
+    ) { innerPadding ->
+        Box(modifier = Modifier.padding(innerPadding)) {
+            content()
+        }
+    }
+}
+
+@Composable
+fun TopBar(navigationTitle: String, haveBackButton: Boolean, navController: NavController) {
+    TopAppBar(
+        title = { Text(navigationTitle) },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onPrimary,
+        navigationIcon = {
+            if (haveBackButton) {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(imageVector = Icons.Default.ArrowBack,"")
                 }
-            )
-        },
-        content = { content() }
+            }
+        }
     )
+}
+
+@Composable
+fun BottomBar() {
+    val selectedIndex = remember { mutableStateOf(0) }
+    BottomNavigation(elevation = 10.dp) {
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Home,"")
+        },
+            label = { Text(text = "Home") },
+            selected = (selectedIndex.value == 0),
+            onClick = {
+                selectedIndex.value = 0
+            })
+
+        BottomNavigationItem(icon = {
+            Icon(imageVector = Icons.Default.Person,"")
+        },
+            label = { Text(text = "Profile") },
+            selected = (selectedIndex.value == 2),
+            onClick = {
+                selectedIndex.value = 2
+            })
+    }
 }
